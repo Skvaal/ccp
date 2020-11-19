e537b5c372322dd93d174f3e50ff830a29e709b4
Vadim Skvortsov <laavks@gmail.com>
2020-11-20 00:01:54 +0300

    Third with cors

--- ccp_back/index.js
+++ ccp_back/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 app.use(express.json());
+app.use(cors());
 
 app.post('/main', require('./methods/main').main);
 app.put('/add_wallets', require('./methods/add_wallets').main);
